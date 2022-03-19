cb7df97843bd7123ef2781b418cf9bd80ac62f2c
usama shafique <96255306+Ussamashafique@users.noreply.github.com>
2022-03-19 04:39:53 -0700

    Add files via upload

---
+++ kidney.php
@@ -0,0 +1,248 @@
+<?php
+    
+
+    include('../header.php');
+?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+ <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="../css/orhto.css">
+    <title>Orthopedic</title
+
+    >
+    <div class="container-fluid">
+    <div class="row ">
+      <form class="col-lg-12 md-6   ">
+  <div id="iconn" > <i class="fas fa-kidney">Institute of Musculoskeletal Science & Orthopaedics </i></h></div>
+<span id="h11">
+   </span>
+   
+    	           
+	
+</form>
+</div>
+</div>
+
+ <div class="container">
+            <div class="row">
+                <div class="lg-12 sm-4">
+
+
+	<div id="child">
+        <h2>Overview</h2>
+       
+<p style="text-align:justify;">Kidneys are very versatile organs, and most people can manage perfectly well with only 15% kidney function. However, in the case of complete kidney failure, our kidneys are no longer able to excrete the toxic waste products into the urine. The constant ratios of the waste products in the bloodstream increases and we become ill. There are two types of kidney failure. In acute kidney failure, which can occur because of a sudden trauma to the kidneys, the kidneys may stop working for a short while and then partly or wholly recover. Chronic renal failure is a progressive condition where the kidneys become irreversibly damaged over a long period, often many years. This condition can develop because of an infection, diabetes, hypertension, or inherited abnormalities. Advanced chronic renal failure is known as end-stage renal disease, where your kidneys are no longer able to function.</p>
+<ul>
+  <p>Symptoms of Kidney Failure </p>
+  <li> Itching</li>
+  <li>Nausea, vomiting, pigmentation and easy bruising</li>
+  <li>Reduced sexual function</li>
+  <li>Abnormal build-up of fluid in the ankles and legs</li>
+  <li> Chest pain</li>
+  <li> Breathlessness</li>
+  <li> Cramps and twisting</li>
+</ul>
+<br>
+<p style="text-align:justify;">When your kidneys are no longer working properly, there are treatments such as haemodialysis and peritoneal dialysis which can filter the waste products from your body. However, dialysis does not perform many of the other vital functions such as stimulating the production of red blood cells. In this case, only a kidney transplant can work. </p>
+<br><br>
+
+<h2> Dietry Managment</h2><br>
+<p  style="text-align:justify;">
+  Five main constituents of the diet have to be included while planning the diet for a patient with chronic renal failure. These are fluid (water), protein, sodium, potassium (salts), and phosphorus.</p>
+ 
+<br>
+<h2> Physiotherapy</h2><br>
+
+<p  style="text-align:justify;"> The goals of physiotherapy for organ transplant patients are to: Minimise post-operative pulmonary <br>complications; Minimise the deconditioning effects of bed rest; Increase general<br> strength, endurance and flexibility; Develop and reinforce a habit of regular exercise.</p>
+
+<br>
+<p  style="text-align:justify;">When your kidneys are no longer working properly, there are treatments such as haemodialysis and peritoneal dialysis which can filter the waste products from your body. However, dialysis does not perform many of the other vital functions such as stimulating the production of red blood cells. In this case, only a kidney transplant can work. </p>
+</div>
+</div>
+<h2>Dietry Managment</h2><br>
+<p style="font-size: 15px; text-align: justify;">
+  
+  Five main constituents of the diet have to be included while planning the diet for a patient with chronic renal failure. These are fluid (water), protein, sodium, potassium (salts), and phosphorus.</p>
+ 
+<br>
+<div class="container">
+    <div class="row  ">
+
+<div class=" Spondylitis d-flex bd-highlight justify-content-center align-item-center ">
+  <div class="p-2 w-50 bd-highlight">
+
+
+
+
+<h2> Physiotherapy</h2><br>
+
+ <p style="font-size:14px;text-align: justify;"> The goals of physiotherapy for organ transplant patients are to: Minimise post-operative pulmonary complications; Minimise the deconditioning effects of bed rest; Increase general strength, endurance and flexibility; Develop and reinforce a habit of regular exercise.</p>
+
+
+<!-- </div>
+ --></div>
+<div class="p-2 w-50 bd-highlight">
+   
+      <iframe 
+     width="348" height="191" src="https://www.youtube.com/embed/AjCWCsbr3DE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+</div>
+<h2> Dialysis</h2><br>
+
+ <p style="font-size:14px;text-align: justify;"> 
+
+
+A patient with end-stage kidney failure has DIALYSIS and TRANSPLANTATION as the only options available for his/her survival. As and when the patients reach a stage, where their kidneys fail to sustain body functions they have to be put on regular dialysis for keeping them alive. Even if the patient decides to have a transplant, he will need the support of dialysis till the preparations for kidney transplantation are made. Dialysis is a substitute for failed kidneys but does not replace kidney function. Patients on dialysis do not do well in the long run and develop some complications, which are not seen after transplantation. Patients live longer after transplantation than on dialysis. </p>
+
+</div>
+</div>
+
+</div>
+</div>
+
+
+
+
+
+ 
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+
+
+
+
+
+<!-- seach code for first search bar ---->
+<script>
+      $(document).ready(function(){
+          
+          function fetchData(){
+            var s = $("#input").val();
+
+            if (s == '') {
+               $('#dropdown').css('display', 'none');
+            }
+            $.post("assets/auto_complet.php", 
+                  {
+                    s:s
+                  },
+                  function(data, status){
+                      if (data != "not found") {
+                        $('#dropdown').css('display', 'block');
+                        $('#dropdown').html(data);
+                      }
+                  });
+          }
+           $('#input').on('input', fetchData);
+           $("body").on('click', () => {
+             $('#dropdown').css('display', 'none');
+          });
+
+
+      });
+</script>
+
+<!--- end ----->
+
+
+
+
+
+
+</div>
+
+
+<div style="width: 99%; height: 7rem ; background-color:#f5f5f5;">
+<h1 style="text-align: center; font-size: 40px;margin-top: 80px;margin-bottom: 50px"> Our Experts</h1>
+</div>
+
+      <article id="a">
+      <div class="searchBox">
+            <!-- <input class="searchInput"type="text" name="" placeholder="Search"  id="input" 
+                               autocomplete="off">
+            <button class="searchButton" href="#">
+                <i class="material-icons">
+                    search
+                </i>
+            </button> -->
+          <div class="search-box">
+    <button class="btn-search"><i class="fas fa-search"></i></button>
+    <input type="text" class="input-search" placeholder="Type to Search..." autocomplete="off" id="input">
+  </div>
+
+
+            
+     <br>
+     
+      <ul class="dropdown" id="dropdown"></ul>
+        </div>
+    </article>
+
+<?php 
+
+$conn = mysqli_connect("localhost","root","","doctor_appointment") or die("connection failed");
+ $query = "SELECT * FROM doctor_table Limit 6" ;
+ $result = mysqli_query($conn,$query) or die("query failed");
+     if(mysqli_num_rows($result)>0)
+{
+
+
+?>
+
+<article id="a">
+<div class="container " >
+
+
+            <div class="row " >
+                 <?php 
+while($row = mysqli_fetch_assoc($result))
+{
+?>
+                
+                <div class="col-lg-4 col-md-6 col-sm-6">
+
+
+
+
+<div class="card" style="width: 28rem; margin-left: 2rem;font-size: 15px;margin-top: 105px;">
+        <div class="text-center"> 
+          
+ 
+             <img class="card-img-top rounded"   src="<?php echo $row['doctor_profile_image'] ?>" alt="Card image cap" style="width: 120px; height:130px"  >    
+  
+
+ <h3 style="width:40rem; margin-left: -60px "> <?php echo $row['doctor_degree']?></h3><br>
+  <div class="card-body">
+    <p class="card-text"><?php echo $row['doctor_expert_in']?> </p>
+            <a href="Cardiologydoctor1.php?id=<?php echo $row['doctor_id']?>" class="btn"> Learn More  <span class="fas fa-chevron-right"></span> </a>
+
+  </div>
+</div>
+</div>
+
+
+
+</div>
+<?php  }?>
+
+
+</div>
+</div>
+
+
+      </article> 
+
+
+      <?php } ?>
+ <!-- footer section starts  -->
+
+<?php
+include('footer.php');
+?>
+
+<!-- footer section ends -->
+</body>
+</html>
---
+++ orthopedic.php
@@ -0,0 +1,383 @@
+<?php 
+include('header.php');
+
+?>
+
+     <link rel="stylesheet" href="css/orhto.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+ <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <div class="row ">
+      <form class="col-lg-12 md-6   ">
+  <div id="iconn" > <i class="fas fa-bone">Institute of Musculoskeletal Science & Orthopaedics </i></h></div>
+<span id="h11">
+   </span>
+   
+    	           
+	
+</form>
+</div>
+</div>
+
+ <div class="container">
+            <div class="row">
+                <div class="lg-12 sm-4">
+
+
+	<div id="child">
+        <h2>Overview</h2>
+       
+<p> Institute of Musculoskeletal Sciences offers comprehensive care for several Orthopaedic afflictions including knee, hip, and joint problems. We are dubbed as the top orthopedic Hospital in India and are equipped with a wide array of specialties ranging from Sports Medicine, Paediatric orthopaedic services, arthritis Diagnosis to Treatment and Pain Management. We focus on providing the highest level of patient care with professional expertise for early mobilization and have taken orthopedic Treatment in Delhi to an altogether different level.
+<br>
+We perform procedures like Computer Assisted Joint Replacement Surgery, Arthroscopic Surgery, Traumatic Orthopaedic Surgery, Hand, Shoulder & Elbow Surgery, Spine Surgery, Articular Surface Replacement Hip Surgery, Foot & Ankle Surgery, Total Knee & Hip Replacement Surgery among several others.
+<br>
+Max Institute of Musculoskeletal Sciences is equipped with a digital Orthopaedic operating suite. The joint implants are planned pre-operatively for perfect size and positioning as per the anatomy of the patient, thereby safeguarding the patients. Our wide range of offerings have made us rank among the best hospitals in India.</p>
+
+<br>
+
+</div>
+</div>
+<h2>Spinal Fractures and Paralysis</h2><br>
+<p style="font-size: 15px;">
+  Spinal fracture (PARALYSIS) or spinal instability is a cause of abnormal movement between two vertebrae. This abnormality can lead to damage or pain in the nerves and spinal cord. Spinal fractures are mostly due to sports injuries, falls, car injuries and gunshots. Symptoms of spinal fracture include numbness, back or neck pain, weakness, muscle spasm, paralysis, tingling and bowel or bladder changes. Doctors conduct X-rays, CT Scan and MRI to diagnose the severity of the situation</p>
+ 
+<br>
+<div class="container">
+    <div class="row  ">
+
+<div class=" Spondylitis d-flex bd-highlight justify-content-center align-item-center ">
+  <div class="p-2 w-50 bd-highlight">
+
+
+
+
+<h2> Spondylitis</h2><br>
+
+ <p style="font-size:14px;text-align: justify;"> Also known as Ankylosing Spondylitis, spondylitis is a result of inflammation of the vertebral joints. This condition develops silently and comes in prominence once fully developed. The three main types of spondylitis include cervical spondylitis, lumbar spondylitis and ankylosing spondylitis.</p>
+
+
+<!-- </div>
+ --></div>
+<div class="p-2 w-50 bd-highlight">
+    <iframe 
+      width="346" height="211"  src="https://www.youtube.com/embed/ZQ8veBNuAeg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ></iframe>
+</div>
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+
+
+
+
+
+ 
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+
+
+
+
+
+
+<!-- seach code for first search bar ---->
+<script>
+      $(document).ready(function(){
+          
+          function fetchData(){
+            var s = $("#input").val();
+
+            if (s == '') {
+               $('#dropdown').css('display', 'none');
+            }
+            $.post("Api/auto_complet.php", 
+                  {
+                    s:s
+                  },
+                  function(data, status){
+                      if (data != "not found") {
+                        $('#dropdown').css('display', 'block');
+                        $('#dropdown').html(data);
+                      }
+                  });
+          }
+           $('#input').on('input', fetchData);
+           $("body").on('click', () => {
+             $('#dropdown').css('display', 'none');
+          });
+
+// $("#input").hover(function(){
+//   $(this).on('input', fetchData);
+//   }, function(){
+//    $('#dropdown').css('display', 'none');
+// });
+
+         // $('#input').on('click', fetchData);
+      });
+</script>
+
+<!--- end ----->
+
+
+
+
+
+
+</div>
+ <div class="container">
+            <div class="row">
+                <div  style="width: 99%; height: 7rem ; background-color:#f5f5f5;">
+    <h2 style="text-align: center; padding-top: 9px;">Conditions:</h2><br> 
+</div>
+                <div class="col-lg-4 col-md-6 col-sm-9">
+
+
+
+ 
+        <div  style="font-size: 15px;" >
+          <h2 >Disc Replacement Surgery</h2><br>
+  <div class="card-body"  >
+
+    <p class="card-text" style="text-align:justify;"> Any unremitting lower back pain that has been there from at least six months and is not getting better from medication may require disc replacement surgery to restore one’s ability to function. The painful disc is replaced with a prosthetic implant with an aim to bring back normal function. There are certain health conditions that can prevent one from getting a disc replacement surgery. These conditions include osteoporosis, spinal tumor, pregnancy, vertebral body fracture and allergy to the materials in the artificial implant</p></div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 col-sm-9">
+ 
+        <div  style="font-size: 15px;">
+          <h2  >Disc Replacement Surgery</h2><br>
+  <div class="card-body" style="text-align:justify;" >
+
+    <p class="card-text" style=""> Any unremitting lower back pain that has been there from at least six months and is not getting better from medication may require disc replacement surgery to restore one’s ability to function. The painful disc is replaced with a prosthetic implant with an aim to bring back normal function. There are certain health conditions that can prevent one from getting a disc replacement surgery. These conditions include osteoporosis, spinal tumor, pregnancy, vertebral body fracture and allergy to the materials in the artificial implant</p></div>
+</div>
+</div> 
+<div class="col-lg-4 col-md-6 col-sm-9">
+ 
+        <div  style="font-size: 15px;" >
+          <h2 >Disc Replacement Surgery</h2><br>
+  <div class="card-body" >
+
+    <p class="card-text" style="text-align:justify;"> Any unremitting lower back pain that has been there from at least six months and is not getting better from medication may require disc replacement surgery to restore one’s ability to function. The painful disc is replaced with a prosthetic implant with an aim to bring back normal function. There are certain health conditions that can prevent one from getting a disc replacement surgery. These conditions include osteoporosis, spinal tumor, pregnancy, vertebral body fracture and allergy to the materials in the artificial implant</p></div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 col-sm-9">
+ 
+        <div  style="font-size: 15px;">
+          <h2  >Disc Replacement Surgery</h2><br>
+  <div class="card-body" >
+
+    <p class="card-text" style="text-align:justify"> Any unremitting lower back pain that has been there from at least six months and is not getting better from medication may require disc replacement surgery to restore one’s ability to function. The painful disc is replaced with a prosthetic implant with an aim to bring back normal function. There are certain health conditions that can prevent one from getting a disc replacement surgery. These conditions include osteoporosis, spinal tumor, pregnancy, vertebral body fracture and allergy to the materials in the artificial implant</p></div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 col-sm-9">
+ 
+        <div  style="font-size: 15px;">
+          <h2 >Disc Replacement Surgery</h2><br>
+  <div class="card-body" >
+
+    <p class="card-text" style="text-align:justify"> Any unremitting lower back pain that has been there from at least six months and is not getting better from medication may require disc replacement surgery to restore one’s ability to function. The painful disc is replaced with a prosthetic implant with an aim to bring back normal function. There are certain health conditions that can prevent one from getting a disc replacement surgery. These conditions include osteoporosis, spinal tumor, pregnancy, vertebral body fracture and allergy to the materials in the artificial implant</p></div>
+</div>
+</div>       
+
+</div>
+</div>
+
+
+<div style="width: 99%; height: 7rem ; background-color:#f5f5f5;">
+<h1 style="text-align: center; font-size: 40px;margin-top: 80px;margin-bottom: 50px"> Our Experts</h1>
+</div>
+
+      <article id="a">
+      <div class="searchBox">
+            <!-- <input class="searchInput"type="text" name="" placeholder="Search"  id="input" 
+                               autocomplete="off">
+            <button class="searchButton" href="#">
+                <i class="material-icons">
+                    search
+                </i>
+            </button> -->
+          <div class="search-box">
+    <button class="btn-search"><i class="fas fa-search"></i></button>
+    <input type="text" class="input-search" placeholder="Type to Search..." autocomplete="off" id="input">
+  </div>
+
+
+            
+     <br>
+     
+      <ul class="dropdown" id="dropdown"></ul>
+        </div>
+    </article>
+
+<!--<div id="searchresult"></div>  -->
+<?php 
+
+$conn = mysqli_connect("localhost","root","","doctor_appointment") or die("connection failed");
+ $query = "SELECT * FROM doctor_table" ;
+ $result = mysqli_query($conn,$query) or die("query failed");
+     if(mysqli_num_rows($result)>0)
+{
+
+
+?>
+
+<article id="a">
+<div class="container " >
+
+
+            <div class="row " >
+                 <?php 
+while($row = mysqli_fetch_assoc($result))
+{
+?>
+                
+                <div class="col-lg-4 col-md-6 col-sm-6">
+
+
+
+
+<div class="card" style="width: 28rem; margin-left: 2rem;font-size: 15px;margin-top: 105px;">
+        <div class="text-center"> 
+          
+ 
+             <img class="card-img-top rounded"   src="<?php echo $row['doctor_profile_image'] ?>" alt="Card image cap" style="width: 120px; height:130px"  >    
+  
+
+ <h3 style="width:40rem; margin-left: -60px "> <?php echo $row['doctor_degree']?></h3><br>
+  <div class="card-body">
+    <p class="card-text"><?php echo $row['doctor_expert_in']?> </p>
+            <a href="Cardiologydoctor1.php?id=<?php echo $row['doctor_id']?>" class="btn"> Learn More  <span class="fas fa-chevron-right"></span> </a>
+
+  </div>
+</div>
+</div>
+
+<?php  }?>
+
+
+</div>
+<!-- <div class="col-lg-4 col-md-6 col-sm-6">
+ 
+   
+    <div class="card" style="width: 28rem; margin-left: 2rem;font-size: 15px;margin-top: 105px;">
+   <div class="text-center">
+  <img class="card-img-top rounded" src="image/dr-junaid-zaffar-cardiologist-lahore_170X170.jpg" alt="Card image cap" style="width: 120px; height:130px ">
+  <h3 style="width:40rem; margin-left: -60px "> <a href="">Orthopedic</a></h3><br>
+  <div class="card-body">
+    <p class="card-text"> Directer & Coordinator<br>Cardiology  cardiosciences, cardiology</p>
+                <a href="contact us.html" class="btn"> Learn More <span class="fas fa-chevron-right"></span> 
+                </a>
+
+  </div>
+</div>    
+</div>
+</div> 
+<div class="col-lg-4 col-md-6 col-sm-6">
+ <div class="card" style="width: 28rem; margin-left: 2rem;font-size: 15px;margin-top: 105px;">
+      <div class="text-center">   
+  <img class="card-img-top rounded" src="image/muhammad-zeeshan-cardiologist-faisalabad-43_80X80.jpg" alt="Card image cap" style="width: 120px; height: 130px">
+   <h3 style="width:40rem; margin-left: -60px "> <a href="">Orthopedic</a></h3><br>
+  <div class="card-body">
+    <p class="card-text"> Directer & Coordinator<br> Cardiology  cardiosciences, cardiology</p>
+                <a href="contact us.html" class="btn"> Learn More <span class="fas fa-chevron-right"></span> 
+                </a>
+
+  </div>
+</div>
+       </div> 
+</div>
+<div class="col-lg-4 col-md-6 col-sm-6">
+ 
+ <div class="card" style="width: 28rem; margin-left: 2rem;font-size: 15px; margin-top: 50px;">
+       <br>
+        <div class="text-center">
+  <img class="card-img-top rounded" src="image/dr-abubakar-maqbool-cardiologist-sialkot-33_170X170.jpg" alt="Card image cap" style="width: 130px; height: 130px ">
+   <h3 style="width:40rem; margin-left: -60px "> <a href="">Orthopedic</a></h3><br>
+  <div class="card-body">
+    <p class="card-text"> Directer & Coordinator<br> Cardiology  cardiosciences, cardiology</p>
+              <a href="contact us.html" class="btn"> Learn More <span class="fas fa-chevron-right"></span> </a>
+  
+  </div>
+</div>
+    </div>    
+</div>
+<div class="col-lg-4 col-md-6 col-sm-6">
+ <div class="card" style="width: 28rem; margin-left: 2rem;font-size: 15px; margin-top: 50px;">
+      <div class="text-center">  
+  <img class="card-img-top rounded" src="image/dr-sumera-naseem-cardiologist-karachi-95_170X170.jpg" alt="Card image cap" style="width: 130px;">
+   <h3 style="width:40rem; margin-left: -60px "> <a href="">Orthopedic</a></h3><br>
+  <div class="card-body">
+    <p class="card-text"> Directer & Coordinator<br> Cardiology  cardiosciences, cardiology</p>
+              <a href="contact us.html" class="btn"> Learn More <span class="fas fa-chevron-right"></span> </a>
+  
+  </div>
+</div>
+</div>       
+</div> -->
+</div>
+</div>
+
+
+      </article> 
+
+
+      <?php } ?>
+ <!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+               <h3>Quick links</h3>     
+            <a href="index.html"> <i class="fas fa-chevron-right"></i> home </a>
+            <a href="services.html"> <i class="fas fa-chevron-right"></i> services </a>
+                <a href="treatment.html"> <i class="fas fa-chevron-right"></i> treatment </a>
+            <a href="about us.html"> <i class="fas fa-chevron-right"></i> about </a>
+            <a href="contact us.html"> <i class="fas fa-chevron-right"></i> Contact Us </a>
+        </div>
+
+        <div class="box">
+            <h3>our Treatments</h3>
+            <a href="#"> <i class="fas fa-chevron-right"></i> dental care </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> Pulmonology</a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> cardiology </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> Orthopedic </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> neurosciences </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> Oncology </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> kidney transplant </a>
+            
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> (048)9230764 </a>
+            <a href="#"> <i class="fas fa-envelope"></i> dhq_teaching@yahoo.com </a>
+           
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> khushab road ,sargodha Pakistan - 40100 </a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         
+            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+            <a href="#"> <i class="fab fa-pinterest"></i> pinterest </a>
+        </div>
+
+    </div>
+
+    <div class="credit">  | all rights reserved </div>
+
+</section>
+
+<!-- footer section ends -->
+</body>
+</html>

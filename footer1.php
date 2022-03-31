2959babfc6988bf784cef8984d643d12d4919571
usama shafique <96255306+Ussamashafique@users.noreply.github.com>
2022-03-31 00:48:27 -0700

    Add files via upload

---
+++ footer1.php
@@ -0,0 +1,53 @@
+<section class="footer">
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" ></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"> </script>
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+ <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <div class="box-container">
+
+        <div class="box">
+               <h3>Quick links</h3>     
+            <a href="admin/home.php" style="text-decoration: none;"> <i class="fas fa-chevron-right"></i> home </a>
+            <a href="admin/services.php" style="text-decoration: none;"> <i class="fas fa-chevron-right"></i> services </a>
+                <a href="admin/treatment.php"style="text-decoration: none;" > <i class="fas fa-chevron-right"></i> treatment </a>
+            <a href="admin/about us.php" style="text-decoration: none;"> <i class="fas fa-chevron-right"></i> about </a>
+            <a href="admin/contact us.php" style="text-decoration: none;"> <i class="fas fa-chevron-right"></i> Contact Us </a>
+        </div>
+
+        <div class="box">
+            <h3>our Treatments</h3>
+            <a href="admin/dentals.php" style="text-decoration: none;"> <i class="fas fa-chevron-right"></i> dental care </a>
+            <a href="admin/Pulmonology.php" style="text-decoration: none;"> <i class="fas fa-chevron-right"></i> Pulmonology</a>
+            <a href="admin/Cardiology.php" style="text-decoration: none;"> <i class="fas fa-chevron-right"></i> cardiology </a>
+            <a href="admin/Orthopedic.php" style="text-decoration: none;"> <i class="fas fa-chevron-right"></i> Orthopedic </a>
+            <a href="admin/neurosciences.php" style="text-decoration: none;"> <i class="fas fa-chevron-right"></i> neurosciences </a>
+            <a href="admin/Oncology.php" style="text-decoration: none;"> <i class="fas fa-chevron-right"></i> Oncology </a>
+            <a href="admin/kidney.php" style="text-decoration: none;"> <i class="fas fa-chevron-right"></i> kidney transplant </a>
+            
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#" style="text-decoration: none;"> <i class="fas fa-phone"></i> (048)9230764 </a>
+            <a href="#" style="text-decoration: none;"> <i class="fas fa-envelope"></i> dhq_teaching@yahoo.com </a>
+           
+            <a href="#" style="text-decoration: none;"> <i class="fas fa-map-marker-alt"></i> khushab road ,sargodha Pakistan - 40100 </a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="https://m.facebook.com/profile.php?id=239246120061590" style="text-decoration: none;"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="https://twitter.com/civilhospamd" style="text-decoration: none;"> <i class="fab fa-twitter"></i> twitter </a>
+         
+            <a href="https://www.instagram.com/civilhospitalamdavad/?hl=en" style="text-decoration: none;" > <i class="fab fa-instagram"></i> instagram </a>
+            <a href="https://pk.linkedin.com/company/civilhospitalkarachi?challengeId=AQHHwWfYgdd5QwAAAX_LPUu_xANbmdDveZubIFx7aXXCsUTPgorQ5bllHl36PTKlCwy7VMvVqNKPfDFWEwJekWhx557iWeL4_Q&submissionId=1b1e3903-f03a-e016-6ce2-1095c138fd5c" style="text-decoration: none;"> <i class="fab fa-linkedin"></i> linkedin </a>
+            <a href="https://in.pinterest.com/civilhospitalw/" style="text-decoration: none;"> <i class="fab fa-pinterest"></i> pinterest </a>
+        </div>
+
+    </div>
+
+    <div class="credit">  | all rights reserved </div>
+
+</section>

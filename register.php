9594a73829af03ccf04d806913158a9e12a842bd
usama shafique <96255306+Ussamashafique@users.noreply.github.com>
2022-03-31 09:42:59 -0700

    Add files via upload

---
+++ register.php
@@ -0,0 +1,227 @@
+<?php
+
+//login.php
+
+include('header.php');
+
+?>
+
+<div class="container">
+	<div class="row justify-content-md-center">
+		<div class="col col-md-6">
+			<span id="message"></span>
+			<div class="card">
+				<div class="card-header">Register</div>
+				<div class="card-body">
+					<form method="post" id="patient_register_form">
+						<div class="form-group">
+							<label>Patient Email Address<span class="text-danger">*</span></label>
+							<input type="text" name="patient_email_address" id="patient_email_address" class="form-control" required autofocus data-parsley-type="email" data-parsley-trigger="keyup" />
+						</div>
+						<div class="form-group">
+							<label>Patient Password<span class="text-danger">*</span></label>
+							<input type="password" name="patient_password" id="patient_password" class="form-control" required  data-parsley-trigger="keyup" />
+						</div>
+						<div class="row">
+							<div class="col-md-6">
+								<div class="form-group">
+									<label>Patient First Name<span class="text-danger">*</span></label>
+									<input type="text" name="patient_first_name" id="patient_first_name" class="form-control" required  data-parsley-trigger="keyup" />
+								</div>
+						<span id="firsterr" style="color: red; font-size: 13px;"></span>
+
+							</div>
+							<div class="col-md-6">
+								<div class="form-group">
+									<label>Patient Last Name<span class="text-danger">*</span></label>
+									<input type="text" name="patient_last_name" id="patient_last_name" class="form-control" required  data-parsley-trigger="keyup" />
+								</div>
+								<span id="lasterr" style="color: red; font-size: 13px;"></span>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-6">
+								<div class="form-group">
+									<label>Patient Date of Birth<span class="text-danger">*</span></label>
+									<input type="text" name="patient_date_of_birth" id="patient_date_of_birth" class="form-control" required  data-parsley-trigger="keyup" readonly />
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="form-group">
+									<label>Patient Gender<span class="text-danger">*</span></label>
+									<select name="patient_gender" id="patient_gender" class="form-control">
+										<option value="Male">Male</option>
+										<option value="Female">Female</option>
+										<option value="Other">Other</option>
+									</select>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-6">
+								<div class="form-group">
+									<label>Patient Contact No.<span class="text-danger">*</span></label>
+									<input type="text" name="patient_phone_no" id="patient_phone_no" class="form-control" required  data-parsley-trigger="keyup" />
+								</div>
+								<span id="errmsg" style="color: red; font-size: 13px;"></span>
+							</div>
+							<div class="col-md-6">
+								<div class="form-group">
+									<label>Patient Maritial Status<span class="text-danger">*</span></label>
+									<select name="patient_maritial_status" id="patient_maritial_status" class="form-control">
+										<option value="Single">Single</option>
+										<option value="Married">Married</option>
+										<option value="Seperated">Seperated</option>
+										<option value="Divorced">Divorced</option>
+										<option value="Widowed">Widowed</option>
+									</select>
+								</div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label>Patient Complete Address<span class="text-danger">*</span></label>
+							<textarea name="patient_address" id="patient_address" class="form-control" required data-parsley-trigger="keyup"></textarea>
+						</div>
+						<div class="form-group text-center">
+							<input type="hidden" name="action" value="patient_register" />
+							<input type="submit" name="patient_register_button" id="patient_register_button" class="btn btn-primary" value="Register" />
+						</div>
+
+						<div class="form-group text-center">
+							<p><a href="login.php">Login</a></p>
+						</div>
+					</form>
+				</div>
+			</div>
+			<br />
+			<br />
+		</div>
+	</div>
+</div>
+
+<?php
+
+include('footer.php');
+
+?>
+<script type="text/javascript">
+$(document).ready(function () {
+  //called when key is pressed in textbox
+  $("#patient_phone_no").keypress(function (e) {
+     //if the letter is not digit then display error and don't type anything
+     if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+        //display error message
+        $("#errmsg").html("Digits Only").show().fadeOut("slow");
+               return false;
+    }
+   });
+});
+</script>
+
+
+<script type="text/javascript">
+	
+$('#patient_first_name').keypress(function (e) {
+        var regex = new RegExp("^[a-zA-Z]+$");
+        var str = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+        if (regex.test(str)) {
+            return true;
+        }
+        else
+        {
+        e.preventDefault();
+        $("#firsterr").html("Alphabets Only").show().fadeOut("slow");
+        return false;
+        }
+    });
+
+
+</script>
+
+
+
+
+<script type="text/javascript">
+	
+$('#patient_last_name').keypress(function (e) {
+        var regex = new RegExp("^[a-zA-Z]+$");
+        var str = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+        if (regex.test(str)) {
+            return true;
+        }
+        else
+        {
+        e.preventDefault();
+        $("#lasterr").html("Alphabets Only").show().fadeOut("slow");
+        return false;
+        }
+    });
+
+
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script>
+
+$(document).ready(function(){
+
+	$('#patient_date_of_birth').datepicker({
+        format: "yyyy-mm-dd",
+        autoclose: true
+    });
+
+	$('#patient_register_form').parsley();
+
+	$('#patient_register_form').on('submit', function(event){
+
+		event.preventDefault();
+
+		if($('#patient_register_form').parsley().isValid())
+		{
+			$.ajax({
+				url:"action.php",
+				method:"POST",
+				data:$(this).serialize(),
+				dataType:'json',
+				beforeSend:function(){
+					$('#patient_register_button').attr('disabled', 'disabled');
+				},
+				success:function(data)
+				{
+					$('#patient_register_button').attr('disabled', false);
+					$('#patient_register_form')[0].reset();
+					if(data.error !== '')
+					{
+						$('#message').html(data.error);
+					}
+					if(data.success != '')
+					{
+						$('#message').html(data.success);
+					}
+				}
+			});
+		}
+
+	});
+
+});
+
+</script>

ff8c764dde7ab7bc9e6fda87507a767344ee68d8
usama shafique <96255306+Ussamashafique@users.noreply.github.com>
2022-04-08 07:37:25 -0700

    Add files via upload

---
+++ admin/oncology.php
@@ -0,0 +1,320 @@
+<?php
+    
+
+    include('header1.php');
+?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+ <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="../css/orhto.css">
+    <title>Oncology</title
+
+    >
+    <div class="container-fluid">
+    <div class="row ">
+      <form class="col-lg-12 md-6   ">
+  <div id="iconn" style="text-align:center"> <i class="fas fa-lungs">Institute of Oncology </i></div>
+<span id="h11">
+   </span>
+   
+    	           
+	
+</form>
+</div>
+</div>
+
+ <div class="container">
+            <div class="row">
+                <div class="lg-12 sm-4">
+
+
+	<div id="child">
+        <h2>Overview</h2>
+       
+<p style="text-align:justify;"> Cancer Care, we offer a holistic and integrated treatment by consolidating views of experts in Surgical Oncology, Radiation Oncology, and Medical Oncology. Our best cancer doctors at Saket, Patparganj, Vaishali, Shalimar Bagh, Gurgaon, Max Smart, Mohali and Bathinda, specialize in treating cancers affecting Breast, Head and Neck, Lung, Gastrointestinal tract and others.
+<br>
+We believe in treating Cancer with a combination of Chemotherapy, Radiation Therapy, Surgery and Targeted Therapy. We are the first facility in northern India to acquire Novalis Tx for IMRT/IGRT, Radiosurgery, HIPEC and SRS/SRT. Additionally, we are equipped with an advanced Da Vinci XI Robotic System for treating complex conditions like cancers of prostate, cervix, colon/rectum, as well as heart tumors. The procedure is the next frontier for minimally invasive surgery.
+
+</div>
+</div>
+<h2>Oncology Diseases</h2><br>
+<p style="font-size: 15px;text-align:justify; margin-top: 8px;">
+  There is a wide range of Oncology diseases that affect the people all around the world. Some of the common ones include:</p>
+  <br>
+  <h3 style="margin-top: 8px; font-weight: bold;"> 
+Acute Lymphocytic Leukemia</h3><br>
+<p style="font-size: 15px;text-align:justify; margin-top: 8px;">
+ 
+
+
+Acute lymphocytic leukemia, a blood and bone marrow cancer, involves a combination of radiation therapy to destroy cancer cells and chemotherapy which our medical team expertly provides.
+</p>
+<br>
+  <h3 style="margin-top: 8px;font-weight: bold;">Acute Myelogenous Leukemia</h3><br>
+<p style="font-size: 15px;text-align:justify;margin-top: 8px;">
+ 
+
+
+
+
+Acute myelogenous leukemia, a blood and bone marrow cancer, involves a combination of radiation therapy to destroy cancer cells and chemotherapy which our medical team expertly provides.
+</p>
+<br>
+  <h3 style="margin-top: 8px;font-weight: bold;"> Basal Cell Carcinoma
+</h3><br>
+<p style="font-size: 15px;text-align:justify; margin-top: 8px;margin-top: 8px;">
+ 
+
+
+
+
+Our renowned experts in skin cancer and dermatologic surgery will develop a personalized treatment plan for patients with basal cell carcinoma.
+</p>
+ 
+ <br>
+  <h3 style="margin-top: 8px;font-weight: bold;"> 
+
+Bile Duct Cancer
+</h3>
+<br>
+<p style="font-size: 15px;text-align:justify;margin-top: 8px;">
+ 
+
+
+
+Cholangiocarcinoma, also known as bile duct cancer, is often treated the same way as hepatocellular carcinoma, the most common form of primary liver cancer that develops in the main type of liver cell.
+</p>
+
+<br>
+<div class="container">
+    <div class="row  ">
+
+<div class=" Spondylitis d-flex bd-highlight justify-content-center align-item-center ">
+  <div class="p-2 w-50 bd-highlight">
+
+
+
+
+<h3 style="font-weight: bold;margin-top: 8px;"> Cystic Fibrosis</h3><br>
+
+ <p style="font-size:14px;text-align: justify;margin-top: 8px;"> 
+It is a genetically inherited disorder that affects the cells which produce mucus, digestive juice and sweat. Its symptoms vary from person to person, but common ones include repeated lung infections, persistent cough with thick mucus, and wheezing.</p>
+
+
+<!-- </div>
+ --></div>
+<div class="p-2 w-50 bd-highlight">
+    
+
+      <iframe width="346" height="211" src="https://www.youtube.com/embed/s0Lemt2PxRQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+
+
+
+
+
+ 
+
+<?php
+include('search.php');
+?>
+
+
+
+
+
+
+</div>
+ <div class="container">
+            <div class="row">
+                <div  style="width: 99%; height: 7rem ; background-color:#f5f5f5;">
+    <h2 style="text-align: center; padding-top: 9px;">Treatments</h2><br> 
+</div>
+                <div class="col-lg-4 col-md-6 col-sm-9">
+
+
+
+ 
+        <div  style="font-size: 15px;" >
+          <h2 >CAR-T Immunotherapy for Cancer</h2><br>
+  <div class="card-body"  >
+
+    <p class="card-text" style="text-align:justify;"> 
+
+
+Cancer immunotherapy with chimeric antigen receptor T-cell therapy, or CAR-T therapy, is FDA-approved for patients with two kinds of blood cancers, adult B-cell non-Hodgkin’s lymphoma or childhood acute lymphoblastic leukemia.
+</p></div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 col-sm-9">
+ 
+        <div  style="font-size: 15px;">
+          <h2  >Chemotherapy</h2><br>
+  <div class="card-body" style="text-align:justify;" >
+
+    <p class="card-text" style=""> 
+
+
+Chemotherapy has the ability to reach cancer that may have spread, even microscopically, throughout the body.
+</p></div>
+</div>
+</div> 
+<div class="col-lg-4 col-md-6 col-sm-9">
+ 
+        <div  style="font-size: 15px;" >
+          <h2 > Radiation</h2><br>
+  <div class="card-body" >
+
+    <p class="card-text" style="text-align:justify;">
+
+
+Radiation therapy uses x-rays, particles, or radioactive seeds to kill cancer cells. Cancer cells grow and divide faster than normal cells in the body. Because radiation is most harmful to quickly growing cells, radiation therapy damages cancer cells more than normal cells. This prevents the cancer cells from growing and dividing, and leads to cell death.</p></div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 col-sm-9">
+ 
+        <div  style="font-size: 15px;">
+          <h2  > Surgery</h2><br>
+  <div class="card-body" >
+
+    <p class="card-text" style="text-align:justify"> 
+
+
+Surgery is a common treatment for many types of cancer. During the operation, the surgeon takes out the mass of cancerous cells (tumor) and some of the nearby tissue. Sometimes, surgery is done to relieve side effects caused by a tumor. </p></div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 col-sm-9">
+ 
+        <div  style="font-size: 15px;">
+          <h2  > Immunotherapy</h2><br>
+  <div class="card-body" >
+
+    <p class="card-text" style="text-align:justify"> 
+
+
+Immunotherapy is a type of cancer treatment that relies on the body's ability to fight infection (immune system). It uses substances made by the body or in a lab to help the immune system work harder or in a more targeted way to fight cancer. This helps your body get rid of cancer cells. 
+     </p></div>
+</div>
+</div>
+
+
+
+
+</div>
+</div>
+
+
+<div style="width: 99%; height: 7rem ; background-color:#f5f5f5;">
+<h1 style="text-align: center; font-size: 40px;margin-top: 80px;margin-bottom: 50px"> Our Experts</h1>
+</div>
+<!-- 
+      <article id="a">
+      <div class="searchBox">
+            <input class="searchInput"type="text" name="" placeholder="Search"  id="input" 
+                               autocomplete="off">
+            <button class="searchButton" href="#">
+                <i class="material-icons">
+                    search
+                </i>
+            </button>
+          <div class="search-box">
+    <button class="btn-search"><i class="fas fa-search"></i></button>
+    <input type="text" class="input-search" placeholder="Type to Search..." autocomplete="off" id="input">
+  </div>
+
+
+            
+     <br>
+     
+      <ul class="dropdown" id="dropdown"></ul>
+        </div>
+    </article> -->
+
+<!--<div id="searchresult"></div>  -->
+<?php 
+
+$conn = mysqli_connect("localhost","root","","doctor_appointment") or die("connection failed");
+ $query = "SELECT * FROM doctor_table Limit 6" ;
+ $result = mysqli_query($conn,$query) or die("query failed");
+     if(mysqli_num_rows($result)>0)
+{
+
+
+?>
+
+<article id="a">
+<div class="container " >
+
+
+            <div class="row " >
+                 <?php 
+while($row = mysqli_fetch_assoc($result))
+{
+?>
+                
+                <div class="col-lg-4 col-md-6 col-sm-6">
+
+
+
+
+<div class="card" style="width: 28rem; margin-left: 2rem;font-size: 15px;margin-top: 105px;">
+        <div class="text-center"> 
+          
+ 
+             <img class="card-img-top rounded"   src="<?php echo $row['doctor_profile_image'] ?>" alt="Card image cap" style="width: 120px; height:130px"  >    
+  
+
+ <h3 style="width:40rem; margin-left: -60px "> <?php echo $row['doctor_degree']?></h3><br>
+  <div class="card-body">
+    <p class="card-text"><?php echo $row['doctor_expert_in']?> </p>
+            <a href="Cardiologydoctor1.php?id=<?php echo $row['doctor_id']?>" class="btn"> Learn More  <span class="fas fa-chevron-right"></span> </a>
+
+  </div>
+</div>
+</div>
+
+
+
+</div>
+<?php  }?>
+
+
+</div>
+</div>
+
+
+      </article> 
+
+<div class="text-center"> 
+<a href="alldoctors.php" class="btn text-center" style="align-self: center;" > Learn More  <span class="fas fa-chevron-right"></span> </a>
+</div>
+    <?php } 
+
+  else 
+    header("location: 404/error.php") ?>
+
+
+
+    
+<?php
+mysqli_close($conn);
+
+
+?>
+ <!-- footer section starts  -->
+
+<?php
+include('footer1.php');
+?>
+
+<!-- footer section ends -->
+</body>
+</html>

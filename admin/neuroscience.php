50be156ea776d5664d351dbcea21060d52e807c6
usama shafique <96255306+Ussamashafique@users.noreply.github.com>
2022-04-08 06:05:18 -0700

    Add files via upload

---
+++ admin/neuroscience.php
@@ -0,0 +1,285 @@
+<?php
+    
+
+    include('header1.php');
+?>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+ <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="../css/orhto.css">
+    <title>neuroscience</title
+
+    >
+    <div class="container-fluid">
+    <div class="row ">
+      <form class="col-lg-12 md-6   ">
+  <div id="iconn" style="text-align:center"> <i class="fas fa-brain">Institute of Neuroscience </i></div>
+<span id="h11">
+   </span>
+   
+    	           
+	
+</form>
+</div>
+</div>
+
+ <div class="container">
+            <div class="row">
+                <div class="lg-12 sm-4">
+
+
+	<div id="child">
+        <h2>Overview</h2>
+       
+<p style="text-align:justify;"> The Institute of Neurosciences provides round-the-clock, comprehensive Diagnostic and Therapeutic Neurology services, according to evidence based protocols set as per internationally accepted guidelines. Our casualty services are available round-the-clock for diagnosis and early management of Neurological emergencies like Status Epilepticus, Stroke and Neuromuscular Weakness, Encephalitis, Meningitis, other Neuroinfectious diseases and behaviour changes.
+
+
+
+</div>
+</div>
+<h2>The Institute Houses Four Departments: </h2><br>
+<p style="font-size: 15px;text-align:justify; margin-top: 8px;">
+
+<ul>
+    <li>
+        
+    </li>
+</ul>
+</p>
+  <br>
+  <h3 style="margin-top: 8px; font-weight: bold;"> Asthma</h3><br>
+<p style="font-size: 15px;text-align:justify; margin-top: 8px;">
+ 
+It is a lung disorder due to which the airways get inflamed, narrow and swollen resulting in breathing issues. Some of the common symptoms include wheezing, coughing, shortness of breath and tightness in the chest.</p>
+<br>
+  <h3 style="margin-top: 8px;font-weight: bold;"> Chronic Obstructive Pulmonary Disease</h3><br>
+<p style="font-size: 15px;text-align:justify;margin-top: 8px;">
+ 
+
+Also known as COPD, Chronic Obstructive Pulmonary Disease refers to a group of lung diseases that lead to blockage in the airflow causing breathing issues. The common conditions in COPD are chronic bronchitis and emphysema. Common signs and symptoms are wheezing, chronic cough, bluish fingernails, unexplained weight loss and shortness of breath.</p>
+<br>
+  <h3 style="margin-top: 8px;font-weight: bold;"> Tuberculosis
+</h3><br>
+<p style="font-size: 15px;text-align:justify; margin-top: 8px;margin-top: 8px;">
+ 
+
+Tuberculosis is a contagious disease and can spread when the patient sneezes or coughs. Common symptoms are pain in the chest, fatigue, fever, chronic cough, shortness of breath, night sweats among others.</p>
+ 
+ <br>
+  <h3 style="margin-top: 8px;font-weight: bold;"> 
+Lung Cancer
+</h3>
+<br>
+<p style="font-size: 15px;text-align:justify;margin-top: 8px;">
+ 
+
+This is the most serious lung disease during which there is an overproduction of abnormal cells in the lung that leads to the formation of a malignant tumour. The common symptoms are cough, chest pain, weight loss and wheezing.<br>
+<br>
+Most of the pulmonary diseases have similar symptoms. This is the reason that the person who experiences any of them must visit the best pulmonology hospital for an accurate diagnosis. After the diagnosis, the doctors can suggest the treatment options which suit the condition.</p>
+
+<br>
+<div class="container">
+    <div class="row  ">
+
+<div class=" Spondylitis d-flex bd-highlight justify-content-center align-item-center ">
+  <div class="p-2 w-50 bd-highlight">
+
+
+
+
+<h3 style="font-weight: bold;margin-top: 8px;"> Cystic Fibrosis</h3><br>
+
+ <p style="font-size:14px;text-align: justify;margin-top: 8px;"> 
+It is a genetically inherited disorder that affects the cells which produce mucus, digestive juice and sweat. Its symptoms vary from person to person, but common ones include repeated lung infections, persistent cough with thick mucus, and wheezing.</p>
+
+
+<!-- </div>
+ --></div>
+<div class="p-2 w-50 bd-highlight">
+    
+
+      <iframe width="346" height="211" src="https://www.youtube.com/embed/s0Lemt2PxRQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+</div>
+
+</div>
+</div>
+
+</div>
+</div>
+
+
+
+
+
+ 
+
+<?php
+include('search.php');
+?>
+
+
+
+
+
+
+</div>
+ <div class="container">
+            <div class="row">
+                <div  style="width: 99%; height: 7rem ; background-color:#f5f5f5;">
+    <h2 style="text-align: center; padding-top: 9px;">Treatments</h2><br> 
+</div>
+                <div class="col-lg-4 col-md-6 col-sm-9">
+
+
+
+ 
+        <div  style="font-size: 15px;" >
+          <h2 >Lifestyle Changes</h2><br>
+  <div class="card-body"  >
+
+    <p class="card-text" style="text-align:justify;"> For conditions like asthma and COPD, doctors suggest various lifestyle changes which can effectively manage the symptoms and reduce the harmful effect. Some of the changes are quitting smoking, eating a healthy diet, avoiding excessive heat and cold, learning breathing exercises among others.</p></div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 col-sm-9">
+ 
+        <div  style="font-size: 15px;">
+          <h2  >Medications</h2><br>
+  <div class="card-body" style="text-align:justify;" >
+
+    <p class="card-text" style=""> Several medications are also prescribed to manage the disease and stop its progression. For instance, doctors prescribe quick-relief drugs for asthma attacks.</p></div>
+</div>
+</div> 
+<div class="col-lg-4 col-md-6 col-sm-9">
+ 
+        <div  style="font-size: 15px;" >
+          <h2 > Pulmonary Mending</h2><br>
+  <div class="card-body" >
+
+    <p class="card-text" style="text-align:justify;">
+It is a program through which doctors help the patient in managing the symptoms and improve their quality of life through various exercises and awareness sessions at lungs hospital.</p></div>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 col-sm-9">
+ 
+        <div  style="font-size: 15px;">
+          <h2  > Surgery</h2><br>
+  <div class="card-body" >
+
+    <p class="card-text" style="text-align:justify"> 
+This treatment option is used in severe pulmonary conditions like lung cancer. Some people suffering from serious COPD or cystic fibrosis have to go through lung transplant to treat the condition.</p></div>
+</div>
+</div>
+     
+
+</div>
+</div>
+
+
+<div style="width: 99%; height: 7rem ; background-color:#f5f5f5;">
+<h1 style="text-align: center; font-size: 40px;margin-top: 80px;margin-bottom: 50px"> Our Experts</h1>
+</div>
+
+      <article id="a">
+      <div class="searchBox">
+            <!-- <input class="searchInput"type="text" name="" placeholder="Search"  id="input" 
+                               autocomplete="off">
+            <button class="searchButton" href="#">
+                <i class="material-icons">
+                    search
+                </i>
+            </button> -->
+          <div class="search-box">
+    <button class="btn-search"><i class="fas fa-search"></i></button>
+    <input type="text" class="input-search" placeholder="Type to Search..." autocomplete="off" id="input">
+  </div>
+
+
+            
+     <br>
+     
+      <ul class="dropdown" id="dropdown"></ul>
+        </div>
+    </article>
+
+<!--<div id="searchresult"></div>  -->
+<?php 
+
+$conn = mysqli_connect("localhost","root","","doctor_appointment") or die("connection failed");
+ $query = "SELECT * FROM doctor_table Limit 6" ;
+ $result = mysqli_query($conn,$query) or die("query failed");
+     if(mysqli_num_rows($result)>0)
+{
+
+
+?>
+
+<article id="a">
+<div class="container " >
+
+
+            <div class="row " >
+                 <?php 
+while($row = mysqli_fetch_assoc($result))
+{
+?>
+                
+                <div class="col-lg-4 col-md-6 col-sm-6">
+
+
+
+
+<div class="card" style="width: 28rem; margin-left: 2rem;font-size: 15px;margin-top: 105px;">
+        <div class="text-center"> 
+          
+ 
+             <img class="card-img-top rounded"   src="<?php echo $row['doctor_profile_image'] ?>" alt="Card image cap" style="width: 120px; height:130px"  >    
+  
+
+ <h3 style="width:40rem; margin-left: -60px "> <?php echo $row['doctor_degree']?></h3><br>
+  <div class="card-body">
+    <p class="card-text"><?php echo $row['doctor_expert_in']?> </p>
+            <a href="Cardiologydoctor1.php?id=<?php echo $row['doctor_id']?>" class="btn"> Learn More  <span class="fas fa-chevron-right"></span> </a>
+
+  </div>
+</div>
+</div>
+
+
+
+</div>
+<?php  }?>
+
+
+</div>
+</div>
+
+
+      </article> 
+<div class="text-center"> 
+<a href="alldoctors.php" class="btn text-center" style="align-self: center;" > Learn More  <span class="fas fa-chevron-right"></span> </a>
+</div>
+
+     <?php } 
+
+  else 
+    header("location: 404/error.php") ?>
+
+
+
+    
+<?php
+mysqli_close($conn);
+
+
+?>
+ <!-- footer section starts  -->
+
+<?php
+include('footer1.php');
+?>
+
+<!-- footer section ends -->
+</body>
+</html>
